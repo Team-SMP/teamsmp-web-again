18673ce8dbd9c0ea10c1520a27bcf6dfc740a77e
JunglTemple <tubikardu@gmail.com>
2024-04-27 19:00:40 +0100

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teamsmp-new.vercel.app',
   output: 'hybrid',
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: vercel()
 });
