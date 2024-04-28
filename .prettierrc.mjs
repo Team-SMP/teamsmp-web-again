1d6e1e7436a39e00d8a1181602c9597ccab73c1c
JunglTemple <tubikardu@gmail.com>
2024-04-28 09:35:52 +0100

    Accidentaly format everything with Prettier + add VS Code tasks

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,13 +2,17 @@
 
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
-      },
-    ],
-  };
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+	tabWidth: 4,
+	trailingComma: "es5",
+	semi: true,
+	useTabs: true,
+};
--- src/components/temp.vue
+++ src/components/temp.vue
@@ -1,27 +1,27 @@
 <script>
 export default {
-  data() {
-    return {
-      mcstatus: null // Initialize mcstatus as null initially
-    };
-  },
-  mounted() {
-    fetch('https://api.mcstatus.io/v2/status/java/teamsmp.spdns.eu:33221')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        // Assign response data to mcstatus
-        this.mcstatus = data;
-      })
-		.catch(error => {
-			console.error('There was an issue GET\'ting: ', error);
+	data() {
+		return {
+			mcstatus: null, // Initialize mcstatus as null initially
+		};
+	},
+	mounted() {
+		fetch("https://api.mcstatus.io/v2/status/java/teamsmp.spdns.eu:33221")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				// Assign response data to mcstatus
+				this.mcstatus = data;
+			})
+			.catch((error) => {
+				console.error("There was an issue GET'ting: ", error);
 			});
-		}
-	};
+	},
+};
 </script>
 
 <template>
