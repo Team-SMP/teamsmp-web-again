38aef64e738011eb6c852cc201c8a7fc5c832a56
JunglTemple <tubikardu@gmail.com>
2024-04-28 09:14:18 +0100

    Semi-responsiveness

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+// Prettier config
+
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+  };
